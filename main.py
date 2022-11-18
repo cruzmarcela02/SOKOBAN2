30e1373242834b2576ac84cfdeb5eb8e2b76a73b
mcruz <mjcruz@fi.uba.ar>
2022-11-18 01:40:29 -0300

    Terminamos?

--- main.py
+++ main.py
@@ -9,8 +9,31 @@ IMAGENES = {
     '.': 'img/goal.gif',
     ' ': 'img/ground.gif'
 }
-PIXELES = 64
+PX = 64
 COMILLA = "'"
+
+def perfeccionar_grilla(niveles, dimension):
+    '''
+        Recibe el diccionario de niveles y el de dimension:
+        Si las cadena de caracteres no tienen el ancho maximo, se rellena con ' ' 
+    '''
+    levels = {}
+    for clave in niveles:
+
+        levels[clave]=[]
+            
+        for cadena in niveles[clave]:
+
+            if len(cadena) < dimension[clave]:
+
+                agregar = dimension[clave] - len(cadena)
+                nueva_cadena = cadena + agregar * ' '
+                levels[clave].append(nueva_cadena)
+
+            else:
+                levels[clave].append(cadena)
+    
+    return levels
 
 def cargar_niveles(archivo):
     '''
@@ -50,39 +73,11 @@ def cargar_niveles(archivo):
                         
                         if len(valor) >= dimension[nivel]:
                             dimension[nivel] = len(valor)
-        
+            
             levels = perfeccionar_grilla(niveles_archivo,dimension)
         return levels
     except IOError as err:
         print(f'IO error: {err}')
-
-
-def perfeccionar_grilla(niveles, dimension):
-    '''
-        Recibe 2 diccionarios:
-            1ro contiene los niveles del archivo
-            2do contiene el ancho maximo de cada nivel
-        Si las cadena de caracteres no tienen el ancho maximo, se rellena con ' ' 
-    '''
-    levels = {}
-    for clave in niveles:
-
-        levels[clave]=[]
-            
-        for cadena in niveles[clave]:
-
-            if len(cadena) < dimension[clave]:
-
-                agregar = dimension[clave] - len(cadena)
-                nueva_cadena = cadena + agregar * ' '
-                levels[clave].append(nueva_cadena)
-
-            else:
-                levels[clave].append(cadena)
-    
-    return levels
-
-print(cargar_niveles('niveles.txt'))
 
 def cargar_teclas(archivo):
     '''
@@ -126,24 +121,19 @@ def cargar_teclas(archivo):
     except IndexError as err_2:
         print(f'IndexError: {err_2} - El archivo puede estar mal redactado')
 
-
-
 def amoldar_ventana(grilla):
     '''
         Se calcula la dimension de la ventana, segun la grilla a jugar:
-        Las imgs son de 64 px
-        ancho_ventana = Cantidad de columnas por 64 
-        Idem para el Alto de la pantalla
+        ancho_ventana = Cantidad de columnas por 64 (Idem para el Alto de la pantalla)
     '''
     ancho_ventana, alto_ventana =  soko.dimensiones(grilla)
-    return ancho_ventana*64, alto_ventana*64
+    return ancho_ventana*PX, alto_ventana*PX
 
-def detectar_movimiento(tecla, click):
+def detectar_accion(tecla, click):
     '''
-        Se devuelve la direccion en la que se movera segun la tecla.
-        Si la letra no pertenece a las mias del movimiento. Retorna None
+        Devuelve la accion a realizar segun la tecla.
+        Si la letra no esta en el diccionario retorna None
     '''
-
     if tecla in click:
         return click[tecla]
     return
@@ -156,7 +146,6 @@ def juego_crear(archivo_niveles, archivo_teclas):
             - E inicializamos el primer nivel
         retornamos un diccionario
     '''
-
     levels = cargar_niveles(archivo_niveles)
     teclas = cargar_teclas(archivo_teclas)
 
@@ -168,21 +157,18 @@ def juego_crear(archivo_niveles, archivo_teclas):
         'grilla': grilla,
         'ANCHO' : ancho,
         'ALTO' : alto,
-        'nivel_nro' : 1,
+        'lvl_nro' : 1,
     }
 
     return juego
 
-
 def juego_actualizar(grilla, juego, tecla):
     '''
-        El jugador se mueve en la grilla
-        Cuando se gana cambiamos de grilla
-        Si reiniciamos la grilla vuelve a su estado inicial
-        Si nos movemos y seguimos jugando retorna un clon de la grilla (modificada)
+        Obtenemos la indicacion, puede:
+            Moverse o Reiniciar el nivel 
+        Si gana el nivel avanza al siguiente, si no se actualiza la grilla en forma de clon
     '''
-    
-    indicacion = detectar_movimiento(tecla,juego['teclas'])
+    indicacion = detectar_accion(tecla,juego['teclas'])
     
     grilla = soko.mover(grilla, indicacion)
 
@@ -191,26 +177,35 @@ def juego_actualizar(grilla, juego, tecla):
     
     if indicacion == 'REINICIAR':
         return reiniciar(grilla,juego)
-    
+
+    return grilla
+
+def reiniciar(grilla,juego):
+    '''
+        Reinicia el nivel, vuelve a la grilla inicial del nivel
+    '''
+    nro = juego['lvl_nro']
+    levels = juego ['levels']
+    grilla = levels[f'Level {nro}']
+
     return grilla
 
 def cambiar_de_nivel(grilla,juego):
     '''
         Si el nivel esta ganado:
-            - incrementa un nivel
+            - incrementa un nro de nivel
             - devuelve la grilla del nuevo nivel
             - modifica y reasigna las medidas de la ventana
             - si no se gano, devuelve la misma grilla
     '''
-    
     if soko.juego_ganado(grilla):
-        juego['nivel_nro'] = juego.get('nivel_nro',0) + 1
-        nro = juego['nivel_nro']
+        juego['lvl_nro'] = juego.get('lvl_nro',0) + 1
+        nro = juego['lvl_nro']
         levels = juego ['levels']
         grilla = levels[f'Level {nro}']
         juego['ANCHO'], juego['ALTO'] = amoldar_ventana(grilla)
-    return grilla
 
+    return grilla
 
 def mostrar(grilla):
     '''
@@ -221,30 +216,18 @@ def mostrar(grilla):
     for f in range(filas):
         for c in range(columnas):
             caracter = grilla[f][c]
-            gamelib.draw_image(IMAGENES[' '], c*PIXELES, f*PIXELES)
+            gamelib.draw_image(IMAGENES[' '], c*PX, f*PX)
 
             if caracter in IMAGENES:
-                gamelib.draw_image(IMAGENES[caracter], c*PIXELES, f*PIXELES)
+                gamelib.draw_image(IMAGENES[caracter], c*PX, f*PX)
                 
             if soko.hay_objetivo(grilla,c,f) and soko.hay_jugador(grilla,c,f):
-                gamelib.draw_image(IMAGENES['@'], c*PIXELES, f*PIXELES)
-                gamelib.draw_image(IMAGENES['.'], c*PIXELES, f*PIXELES)
+                gamelib.draw_image(IMAGENES['@'], c*PX, f*PX)
+                gamelib.draw_image(IMAGENES['.'], c*PX, f*PX)
 
             if soko.hay_objetivo(grilla,c,f) and soko.hay_caja(grilla,c,f):
-                gamelib.draw_image(IMAGENES['$'], c*PIXELES, f*PIXELES)
-                gamelib.draw_image(IMAGENES['.'], c*PIXELES, f*PIXELES)
-
-
-'''
-    Reinicia el nivel, vuelve a la grilla inicial del nivel
-'''
-def reiniciar(grilla,juego):
-
-    nro = juego['nivel_nro']
-    levels = juego ['levels']
-    grilla = levels[f'Level {nro}']
-
-    return grilla
+                gamelib.draw_image(IMAGENES['$'], c*PX, f*PX)
+                gamelib.draw_image(IMAGENES['.'], c*PX, f*PX)
 
 def main():
     # Inicializar el estado del juego
